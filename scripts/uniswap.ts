96008f33209f7ed65dc2faf0a09d6147703e7838
LiYuqing <im.yuqlee@gmail.com>
2022-03-23 22:21:12 +0800

    Fix uniswap

--- scripts/uniswap.ts
+++ scripts/uniswap.ts
@@ -6,34 +6,32 @@ async function main() {
 
   const UniswapV2Factory = await ethers.getContractFactory("UniswapV2Factory");
 
-  // Attach factory exmaple
-  // const factory = await UniswapV2Factory.attach("0xd66c174a322eF2Ca6EabDFECaFbB2826Ccfd5cbf");
-
   // Deploy factory
-  const factory = await UniswapV2Factory.deploy(deployer.address);
+  // const factory = await UniswapV2Factory.deploy(deployer.address);
+  // await factory.deployed();
+
+  // Attach factory exmaple
+  const factory = await UniswapV2Factory.attach("0xd66c174a322eF2Ca6EabDFECaFbB2826Ccfd5cbf");
   console.log("UniswapV2Factory address: " + factory.address);
 
   const UniswapV2Router02 = await ethers.getContractFactory(
     "UniswapV2Router02"
   );
+  // Deploy router
+  // const router = await UniswapV2Router02.deploy(factory.address, deployer.address);
+  // await router.deployed();
 
   // Attach router exmaple
-  // const router = await UniswapV2Router02.attach("0xd66c174a322eF2Ca6EabDFECaFbB2826Ccfd5cbf");
-
-  // Deploy router
-  const router = await UniswapV2Router02.deploy(
-    factory.address,
-    deployer.address
-  );
+  const router = await UniswapV2Router02.attach("0xd66c174a322eF2Ca6EabDFECaFbB2826Ccfd5cbf");
   console.log("UniswapV2Router02 address: " + router.address);
 
   const UniswapMVMRouter = await ethers.getContractFactory("UniswapMVMRouter");
+  // Deploy mvm router
+  // const mvmRouter = await UniswapMVMRouter.deploy(router.address);
+  // await mvmRouter.Deploy()
 
   // Attach mvm router exmaple
-  // const mvmRouter = await UniswapMVMRouter.attach("0x2b60ce6462EDFD5E387f85eF41413aecD1951470");
-
-  // Deploy mvm router
-  const mvmRouter = await UniswapMVMRouter.deploy(router.address);
+  const mvmRouter = await UniswapMVMRouter.attach("0x2b60ce6462EDFD5E387f85eF41413aecD1951470");
   console.log("UniswapMVMRouter address: " + mvmRouter.address);
 }
 
