a88d2e09faca76ef314148aaff8a5ba28c0e3601
LiYuqing <im.yuqlee@gmail.com>
2022-03-24 21:47:37 +0800

    More command for mixin

--- scripts/mixin.ts
+++ scripts/mixin.ts
@@ -1,6 +1,7 @@
 // yarn hardhat run --network quorum scripts/mixin.ts
 import { ethers } from "hardhat";
-const ABI = require('../artifacts/contracts/mixin/bridge.sol/Bridge.json');
+const BridgeABI = require('../artifacts/contracts/mixin/bridge.sol/Bridge.json');
+const RegistryABI = require('../artifacts/contracts/mixin/registry.sol/Registry.json');
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -24,6 +25,8 @@ async function main() {
   // await registry.deployed();
   const registry = Registry.attach("0x43acDdE3f6C3f25c6Cd51f9d70f74607A16E9620");
   console.log("registry deployed to:", registry.address);
+  const registryAbi = new ethers.Contract(registry.address, RegistryABI.abi, deployer);
+  console.log("registry user", await registryAbi.users("0x0215fD72e01e806AeD843F3a0161A0c003969421"));
 
   // publish contract use bot: 7000103716
   // user_id: 2f930b66-be1e-3a69-9b1a-03ba727e05d3
@@ -34,8 +37,9 @@ async function main() {
   let bridge = Bridge.attach("0xBb993F08B7982055D5C2e438832ef5505Eb35Ac9");
 
   // await bridge.bind("0xce9Ccf54865Fbae2dC2b1164Eb3E442e366A4ea2");
-  const bridgeAbi = new ethers.Contract(bridge.address, ABI.abi, deployer);
+  const bridgeAbi = new ethers.Contract(bridge.address, BridgeABI.abi, deployer);
   console.log("Bridged address:", await bridgeAbi.bridges(deployer.address));
+  console.log("Bridged address:", await bridgeAbi.bridges("0x0215fD72e01e806AeD843F3a0161A0c003969421"));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
