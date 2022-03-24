53eec1c26b1084d2da077e7139e9a4834acecb16
LiYuqing <im.yuqlee@gmail.com>
2022-03-24 19:14:29 +0800

    Redeploy mixin

--- scripts/mixin.ts
+++ scripts/mixin.ts
@@ -11,7 +11,7 @@ async function main() {
   const bls = await BLS.attach("0xC164E080B874921F49199de49695dd511d2e5D52");
   // const bls = await BLS.deploy();
   // await bls.deployed();
-  console.log("byteslib deployed to:", bls.address);
+  console.log("bls deployed to:", bls.address);
 
   const Registry = await ethers.getContractFactory("Registry", {
     libraries: {
@@ -20,13 +20,13 @@ async function main() {
   });
   // https://github.com/MixinNetwork/trusted-group/blob/master/mvm/quorum/contracts/mixin.sol#L27
   // ["2f741961cea2e88cfa2680eeaac040d41f41f3fedb01e38c06f4c6058fd7e425", "007d68aef83f9690b04f463e13eadd9b18f4869041f1b67e7f1a30c9d1d2c42c", "2a32fa1736807486256ad8dc6a8740dfb91917cf8d15848133819275be92b673",  "257ad901f02f8a442ccf4f1b1d0d7d3a8e8fe791102706e575d36de1c2a4a40f"]
-  // const registry = await Registry.deploy("0x2f741961cea2e88cfa2680eeaac040d41f41f3fedb01e38c06f4c6058fd7e425007d68aef83f9690b04f463e13eadd9b18f4869041f1b67e7f1a30c9d1d2c42c2a32fa1736807486256ad8dc6a8740dfb91917cf8d15848133819275be92b673257ad901f02f8a442ccf4f1b1d0d7d3a8e8fe791102706e575d36de1c2a4a40f", "0xf7f56b709bfc3a3d948f329699ad6f68");
+  // const registry = await Registry.deploy("0x2f741961cea2e88cfa2680eeaac040d41f41f3fedb01e38c06f4c6058fd7e425007d68aef83f9690b04f463e13eadd9b18f4869041f1b67e7f1a30c9d1d2c42c2a32fa1736807486256ad8dc6a8740dfb91917cf8d15848133819275be92b673257ad901f02f8a442ccf4f1b1d0d7d3a8e8fe791102706e575d36de1c2a4a40f", "0x2f930b66be1e3a699b1a03ba727e05d3");
   // await registry.deployed();
-  const registry = Registry.attach("0x68a99087Bd6e08Ef73A2479C8206308DbD65c3B8");
+  const registry = Registry.attach("0x43acDdE3f6C3f25c6Cd51f9d70f74607A16E9620");
   console.log("registry deployed to:", registry.address);
 
   // publish contract use bot: 7000103716
-  // user_id: 03c19001-04dc-3d12-94aa-af68436e9139
+  // user_id: 2f930b66-be1e-3a69-9b1a-03ba727e05d3
 
   const Bridge = await ethers.getContractFactory("Bridge");
   // const bridge = await Bridge.deploy();
