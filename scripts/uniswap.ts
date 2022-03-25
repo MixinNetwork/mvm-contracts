5c312f0d66f68198aab7a1a535194ad1eddf055a
LiYuqing <im.yuqlee@gmail.com>
2022-03-25 16:39:45 +0800

    Fix uniswap deploy

--- scripts/uniswap.ts
+++ scripts/uniswap.ts
@@ -22,16 +22,16 @@ async function main() {
   // await router.deployed();
 
   // Attach router exmaple
-  const router = await UniswapV2Router02.attach("0xd66c174a322eF2Ca6EabDFECaFbB2826Ccfd5cbf");
+  const router = await UniswapV2Router02.attach("0x0bBe54e2B11D6671238EDB6FB3eD83f73A1ca3dF");
   console.log("UniswapV2Router02 address: " + router.address);
 
   const UniswapMVMRouter = await ethers.getContractFactory("UniswapMVMRouter");
   // Deploy mvm router
   // const mvmRouter = await UniswapMVMRouter.deploy(router.address);
-  // await mvmRouter.Deploy()
+  // await mvmRouter.deployed()
 
   // Attach mvm router exmaple
-  const mvmRouter = await UniswapMVMRouter.attach("0x2b60ce6462EDFD5E387f85eF41413aecD1951470");
+  const mvmRouter = await UniswapMVMRouter.attach("0xBEB55554605314B7ADd09C8344FDAF1Ba81F74A5");
   console.log("UniswapMVMRouter address: " + mvmRouter.address);
 }
 
