8505b11bc01f6b4a76f9ffe0492293e449cbe50f
LiYuqing <im.yuqlee@gmail.com>
2022-03-25 16:41:28 +0800

    More comments for uniswap

--- scripts/uniswap.ts
+++ scripts/uniswap.ts
@@ -33,6 +33,10 @@ async function main() {
   // Attach mvm router exmaple
   const mvmRouter = await UniswapMVMRouter.attach("0xBEB55554605314B7ADd09C8344FDAF1Ba81F74A5");
   console.log("UniswapMVMRouter address: " + mvmRouter.address);
+  // roay 69b2d237-1eb2-3b6c-8e1d-3876e507b263
+  // roay 0xbeb5ecE163ee3fc221Aba1453CA19C423C924326
+  // cnb 965e5c6e-434c-3fa9-b780-c50f43cd955c
+  // cnb 0xe07d1bb9ed65b200b4684a0f3b9268c638c87e98
 }
 
 main().catch((error) => {
