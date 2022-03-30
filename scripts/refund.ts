390678ce97c94c579d7fde95c6f62d14c6872411
YeungKC <I@YeungKC.com>
2022-03-30 18:18:26 +0800

    Deploy refund contract script

---
+++ scripts/refund.ts
@@ -0,0 +1,42 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account: " + deployer.address);
+
+  const BLS = await ethers.getContractFactory("BLS");
+  const bls = await BLS.attach("0xC164E080B874921F49199de49695dd511d2e5D52");
+  // await bls.deployed();
+
+  const Refund = await ethers.getContractFactory("RefundWorker", {
+    libraries: {
+      BLS: bls.address,
+    },
+  });
+
+  // Deploy Refund
+  const refund = await Refund.deploy();
+  await refund.deployed();
+
+  console.log(`Refund address: ${refund.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
