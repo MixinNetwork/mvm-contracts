857aaa01eebf76d9cf64da9c866e2dd7390f11ff
LiYuqing <im.yuqlee@gmail.com>
2022-06-17 16:22:47 +0800

    Fix mixin solidity

--- scripts/mixin.ts
+++ scripts/mixin.ts
@@ -1,7 +1,7 @@
 // yarn hardhat run --network quorum scripts/mixin.ts
 import { ethers } from "hardhat";
 const BridgeABI = require('../artifacts/contracts/mixin/bridge.sol/Bridge.json');
-const RegistryABI = require('../artifacts/contracts/mixin/registry.sol/Registry.json');
+const RegistryABI = require('../artifacts/contracts/mixin/Registry.sol/Registry.json');
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -21,9 +21,9 @@ async function main() {
   console.log("storage deployed to:", storage.address);
 
   const Registry = await ethers.getContractFactory("Registry", {
-    libraries: {
-      BLS: bls.address,
-    },
+   // libraries: {
+   //   BLS: bls.address,
+   // },
   });
   // https://github.com/MixinNetwork/trusted-group/blob/master/mvm/quorum/contracts/mixin.sol#L27
   // ["2f741961cea2e88cfa2680eeaac040d41f41f3fedb01e38c06f4c6058fd7e425", "007d68aef83f9690b04f463e13eadd9b18f4869041f1b67e7f1a30c9d1d2c42c", "2a32fa1736807486256ad8dc6a8740dfb91917cf8d15848133819275be92b673",  "257ad901f02f8a442ccf4f1b1d0d7d3a8e8fe791102706e575d36de1c2a4a40f"]
@@ -68,14 +68,6 @@ async function main() {
   // ROAY invoke f6281e1c-53f7-3125-9cdd-30d5389189f8:69b2d237-1eb2-3b6c-8e1d-3876e507b263:0.114:07b0bf340765cae77b734d82eb8d35229796cebc322e9f04
   // CNB  invoke f6281e1c-53f7-3125-9cdd-30d5389189f8:965e5c6e-434c-3fa9-b780-c50f43cd955c:0.114:07b0bf340765cae77b734d82eb8d35229796cebc322e9f04
   // NXC  invoke f6281e1c-53f7-3125-9cdd-30d5389189f8:66152c0b-3355-38ef-9ec5-cae97e29472a:0.114:07b0bf340765cae77b734d82eb8d35229796cebc322e9f04
-
-  let key = "0x4e7f836e7deab9dd6160277fac3c501fbaf5269e84bf032a3c91137ba8a73d9b";
-  const value = "0x000bbe54e2b11d6671238edb6fb3ed83f73a1ca3df38ed17390000000000000000000000000000000000000000000000000000000005f5e1000000000000000000000000000000000000000000000000000d090add2aceb4b800000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000bf7c691c00de8e3d647d5258ebaec87857a1838d0000000000000000000000000000000000000000000000000000000062a201b90000000000000000000000000000000000000000000000000000000000000002000000000000000000000000155bdfab24f07630c27a3f31634b33f94ec4a634000000000000000000000000cc4623795f07caff65069704d5008778921456a5";
-  await registry.writeValue(storage.address, key, value);
-  console.log("getBytesValue:", await storage.getBytesValue(key));
-  key = ethers.utils.keccak256("0x12345678");
-  await registry.writeValue(storage.address, key, "0x12345678");
-  console.log("getBytesValue:", await storage.getBytesValue(key));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
