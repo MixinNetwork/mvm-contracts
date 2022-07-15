bec6f432c03683ab62c6035a253564d62b4847c6
hundredark <hundredark@gmail.com>
2022-07-15 17:47:56 +0800

    Remove comments

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,17 +1,17 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract Counter { // 计数器合约
-    uint256 public count; // 计数器
-    constructor() { // 构造函数
-        count = 0; // 将计数器置为 0
+contract Counter {
+    uint256 public count;
+    constructor() {
+        count = 0;
     }
 
-    function addOne() public { // 计数器+1 的函数
+    function addOne() public {
         count = count + 1;
     }
 
-    function addAny(uint256 num) public { // 计数器+num 的函数
+    function addAny(uint256 num) public {
         count = count + num;
     }
 }
